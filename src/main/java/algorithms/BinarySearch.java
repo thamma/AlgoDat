ca7e2b8b4c82e1145225ece8329da40637c4b9c9
Dominic Zimmer <s8dozimm@stud.uni-saarland.de>
2015-10-22 17:32:18 +0200

    Still can't get this right...

---
+++ src/main/java/algorithms/BinarySearch.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+public class BinarySearch {
+
+	public static int search(int[] array) {
+		//TODO Implement
+		return -1;
+	}
+	
+}
---
+++ src/test/java/SomethingTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomethingTest {
+
+	@Test
+	public void trivial() {
+		assertTrue(true);
+	}
+
+}
